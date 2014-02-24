108eaa3b109694266224bd460f4c63388ed130c9
getuliojr <gromao@gmail.com>
2014-02-24 16:19:16 -0300

    Fixing JSHint code and some optimization
    Just some fix to pass jshint check and a little optimization. Now it already parse the string and then if it is in the formar 99.99.9999 parses again.

--- src/datepicker/datepicker.js
+++ src/datepicker/datepicker.js
@@ -378,10 +378,10 @@ function ($compile, $parse, $document, $position, dateFilter, datepickerPopupCon
             return undefined;
           } else {
             var dateParts = dateStr.split(/\/|\.|\-/); //check for "/" or "." or "-" to split date
-            if (dateParts.length === 3)
-              var date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
-            else
-              var date = new Date(dateStr);
+            var date = new Date(dateStr);
+            if (dateParts.length === 3){
+              date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+            }  
             if (isNaN(date)) {
               ngModel.$setValidity('date', false);
               return undefined;
